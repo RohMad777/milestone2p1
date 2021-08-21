0c44592b449378cc73080e3c823e4955ec3c5787
RohMad777 <rohmadrock@gmail.com>
2021-08-21 22:02:52 +0700

    update last

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 
 # Create app
 app = Flask(__name__)
-model = pickle.load(open("model/model_decisiontree_clf.pkl", "rb"))
+model = pickle.load(open("model/model_knn_clf.pkl", "rb"))
 
 
 @app.route("/")
@@ -18,7 +18,7 @@ def predict():
     features = [np.array(float_features)]
     prediction = model.predict(features)
 
-    output = {0: "Creditors are not credible", 1: "Credible creditor"}
+    output = {0: "Clients are not credible", 1: "Credible clients"}
 
     return render_template("predict.html",
                            prediction_text="{}".format(output[prediction[0]]))
@@ -29,9 +29,24 @@ def dataSet():
     return render_template('creditcard.html')
 
 
+@app.route('/feature')
+def feature():
+    return render_template('feature.html')
+
+
+@app.route('/confuss')
+def confuss():
+    return render_template('confuss.html')
+
+
 @app.route('/predict')
 def predicts():
     return render_template('predict.html')
+
+
+@app.route('/other')
+def other():
+    return render_template('other.html')
 
 
 if __name__ == "__main__":
